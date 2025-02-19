de6b93584eea7b7cd21164e28082b20a21e27a1d
kuropAqn <kuropan0819@gmail.com>
2025-02-19 10:53:40 +0000

    7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+    redirect_to  post_image_path
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+
+
 end
